ef5c7c3263ca7139e04c351b37c9bfdd1f6ce529
Gaurav Kumar Nayak <91681647+gkn369@users.noreply.github.com>
2025-12-30 10:50:06 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Print N to 1 without loop/print-n-to-1-without-loop.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    void printNos(int N) {
+        // code here
+        if(N == 1){
+            cout << N << " ";
+            return;
+        }
+        cout << N << " ";
+        printNos(N-1);
+        
+    }
+};
