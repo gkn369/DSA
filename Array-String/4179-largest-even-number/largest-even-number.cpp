6add21160799bef1c9b6e099316fbc483c89a325
Gaurav Kumar Nayak <91681647+gkn369@users.noreply.github.com>
2026-01-07 08:37:55 +0530

    Time: 1 ms (16.29%) | Memory: 8.6 MB (39.49%) - LeetSync

---
+++ Array-String/4179-largest-even-number/largest-even-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string largestEven(string s) {
+        int i;
+        int n = s.size();
+        for(int i = n - 1; i >= 0; i--)
+        {
+            if(s[i] == '2')
+                return s.substr(0, i+1); 
+        }
+        return {};
+    }
+};
