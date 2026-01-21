3f3a9fa78619998e58ed77966c6f9c2cc8c1953c
Gaurav Kumar Nayak <91681647+gkn369@users.noreply.github.com>
2026-01-21 20:36:59 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (58.53%) - LeetSync

---
+++ Array-String/496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        stack<int>st;
+        int n = nums2.size();
+        map<int,int>mp;
+        vector<int> ans;
+        for(int i = n-1; i >= 0; i--)
+        {
+            while(!st.empty() && st.top() <= nums2[i])
+            {
+                st.pop();
+            }
+            if(st.empty())
+                mp[nums2[i]] = -1;
+            else
+                mp[nums2[i]] = st.top();
+
+            st.push(nums2[i]);
+        }
+
+        for(int i : nums1)
+        {
+            ans.push_back(mp[i]);
+        }
+
+        return ans;
+    }
+};
