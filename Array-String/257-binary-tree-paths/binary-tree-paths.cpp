1a528b43ea0f89dff6e369a753780423fc6e6620
Gaurav Kumar Nayak <91681647+gkn369@users.noreply.github.com>
2026-01-27 21:53:44 +0530

    Time: 3 ms (30.68%) | Memory: 18.5 MB (12.70%) - LeetSync

---
+++ Array-String/257-binary-tree-paths/binary-tree-paths.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void rootToLeaf(TreeNode *root, string s, vector<string> &ans)
+    {
+        if(root == NULL)
+            return;
+        if(root -> left == NULL && root -> right == NULL)
+        {
+            s += to_string(root -> val);
+            ans.push_back(s);
+        }
+
+        s += to_string(root->val) + "->";
+        rootToLeaf(root -> left, s, ans);
+        rootToLeaf(root -> right, s, ans);
+
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string>ans;
+        rootToLeaf(root, "", ans);
+        return ans;
+    }
+};
