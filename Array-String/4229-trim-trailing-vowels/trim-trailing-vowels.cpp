de3d425d21264b5c2540721e760ef8c279d069ad
Gaurav Kumar Nayak <91681647+gkn369@users.noreply.github.com>
2026-03-02 16:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (100.00%) - LeetSync

---
+++ Array-String/4229-trim-trailing-vowels/trim-trailing-vowels.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string trimTrailingVowels(string s) {
+        int n = s.size();
+        int i = n - 1;
+        while(i >= 0)
+        {
+            if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            {
+                s.erase(i);
+                i--;
+            }
+            else
+                break;
+        }
+        return s;
+    }
+};
