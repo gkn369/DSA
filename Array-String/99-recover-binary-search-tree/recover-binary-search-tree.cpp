129817888703fd14d8f7b01a39683e2ee2a75ae3
Gaurav Kumar Nayak <91681647+gkn369@users.noreply.github.com>
2026-03-25 19:40:54 +0530

    Time: 1 ms (27.26%) | Memory: 60.7 MB (95.34%) - LeetSync

--- Array-String/99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ Array-String/99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -11,31 +11,63 @@
  */
 class Solution {
 public:
-    vector<int> elem;
+    // vector<int> elem;
+    // void inorder(TreeNode *root)
+    // {
+    //     if(root == NULL)
+    //         return;
+    //     inorder(root -> left);
+    //     elem.push_back(root -> val);
+    //     inorder(root -> right);
+    // }
+    // void inorderTrav(TreeNode* root, int &start)
+    // {
+    //     if(root == NULL)
+    //         return;
+    //     inorderTrav(root -> left, start);
+    //     if(root -> val != elem[start])
+    //     {
+    //         root -> val = elem[start];
+    //     }
+    //     start++;
+    //     inorderTrav(root -> right, start);
+    // }
+    // void recoverTree(TreeNode* root) {
+    //     inorder(root);
+    //     sort(elem.begin(), elem.end());
+    //     int start = 0;
+    //     inorderTrav(root, start);
+    // }
+    TreeNode *prev, *start, *mid, *end;
     void inorder(TreeNode *root)
     {
         if(root == NULL)
             return;
+
         inorder(root -> left);
-        elem.push_back(root -> val);
+        if(prev != NULL && (root -> val < prev -> val))
+        {
+            if(start == NULL)
+            {
+                start = prev;
+                mid = root;
+            }
+            else
+            {
+                end = root;
+            }
+        }
+        prev = root;
         inorder(root -> right);
     }
-    void inorderTrav(TreeNode* root, int &start)
-    {
-        if(root == NULL)
-            return;
-        inorderTrav(root -> left, start);
-        if(root -> val != elem[start])
-        {
-            root -> val = elem[start];
-        }
-        start++;
-        inorderTrav(root -> right, start);
-    }
     void recoverTree(TreeNode* root) {
+        start = mid = end = NULL;
+        prev = new TreeNode(INT_MIN);
         inorder(root);
-        sort(elem.begin(), elem.end());
-        int start = 0;
-        inorderTrav(root, start);
+
+        if(start && end)
+            swap(start -> val, end -> val);
+        else if(start && mid)
+            swap(start -> val, mid -> val);
     }
 };
